e2c43020662d3dbaf04fb27a46d8c8e59c34c24f
Lee Gao <leeyuan123@yahoo.com>
2011-10-26 00:34:38 -0300

    dfa now also uses the updated nfa scheme

--- regex/dfa.py
+++ regex/dfa.py
@@ -7,21 +7,27 @@ class dfa(object):
     let edge(s, epsilon) be the set of all states reachable by s with an epsilon transition, then
     For a set of states S(nfa_table), closure(S) = S U (union(for s in closure(S), edge(s, epsilon)))
     """
+
+    def __init__(self, nfa, start, final):
+        self.nfa = nfa
+        self.start = start
+        self.nfa_accept = final
     
-    @classmethod
     @memoize
-    def edge(cls, state, symb='\x00'):
+    def edge(self, state, symb='\x00'):
         """
         The edge function looks through the list of possible transitions
         from an NFA state and returns a set of states that can be reached
         via symbol
         """
-        l = [trans[1] for trans in state.transitions if trans[0] == symb] 
-        return l
-    
-    @classmethod
+        l = []
+        for trans in self.nfa:
+            if trans[0] == state and trans[1] ==symb:
+                l += self.nfa[trans]
+        return l 
+
     @memoize
-    def closure(cls, states):
+    def closure(self, states):
         """
         Closure calculates the epsilon closures of a set of states.
         More specifically, we know that the set of states that can
@@ -40,12 +46,12 @@ class dfa(object):
             on the successively larger closure until the length of the set
             between two iterations no longer changes.
             """
-            edges = dfa.edge(state)
+            edges = self.edge(state)
             last = 0
             while last != len(edges):
                 last = len(edges)
                 for s in edges:
-                    edges += dfa.edge(s)
+                    edges += self.edge(s)
                 edges = list(set(edges))
             return edges
         
@@ -54,8 +60,7 @@ class dfa(object):
             edges += _closure(state)
         return list(set(edges))
     
-    @classmethod
-    def combine(cls, d, symbol):
+    def combine(self, d, symbol):
         """
         Returns every state reachable from the grouped DFA state d
         with a transition of symbol.
@@ -66,22 +71,20 @@ class dfa(object):
         """
         states = []
         for state in d:
-            states += dfa.edge(state, symbol)
-        return dfa.closure(list(set(states)))
+            states += self.edge(state, symbol)
+        return self.closure(list(set(states)))
     
-    @classmethod
-    def is_final(cls, states):
+    def is_final(self, states):
         """
         If any state within states is final, then the DFA state
         is also final.
         """
         for state in states:
-            if state.accept:
+            if state == self.nfa_accept:
                 return True
         return False
     
-    @classmethod
-    def dfa(cls, s1, input):
+    def dfa(self, input):
         """
         Taken from Appel's section on converting an NFA into a DFA.
 
@@ -90,7 +93,7 @@ class dfa(object):
         """
         # states[0] is always non-matching
         # states[1] is the initial 
-        states = [[], dfa.closure([s1])]
+        states = [[], self.closure([self.start])]
         trans = {} # a dictionary of transitions from one state into another
         final = [] # a list of accepting states
         p = 1
@@ -102,7 +105,7 @@ class dfa(object):
                 # being the union of the states.
                 # this way, each character can only go to ONE state for each DFA state,
                 # hence making this automaton by definition a DFA.
-                e = dfa.combine(states[j], c)
+                e = self.combine(states[j], c)
                 if e in states:
                     # if we've already seen it before, add it into the transition dict
                     trans[(j,c)] = states.index(e)
@@ -110,10 +113,16 @@ class dfa(object):
                     # we've encountered a new DFA state. Add it in and add into the transition
                     p += 1
                     states.append(e)
-                    if dfa.is_final(e):
+                    if self.is_final(e):
                         # if any of the nfa states are accepting, then so is this DFA state
                         final.append(p) 
                     trans[(j,c)] = p
                     
             j+= 1
         return trans, final # only the transition dict and the final accept states are useful
+
+if __name__ == "__main__":
+    from nfa import nfa
+    n = nfa("ab*c")
+    trans, accept, start = n.nfa()
+    d = dfa(trans, start, accept)
